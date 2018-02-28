04551463ae625470de8982843fd9c0e14c1d7f13
Jeff Dileo <jeff.dileo@nccgroup.trust>
2018-02-28 07:12:25 -0500

    initial cli interface

---
+++ frida-trustslayer/basic_agent.js
@@ -0,0 +1,544 @@
+
+function okhttp3(cfg) {
+  /* This function will override the OkHttpClient constructor to inject a Proxy
+     into the built OkHttpClient and forcibly set its CertificatePinner to the
+     default blank one. While this can be run through spawn gating, it also
+     supports setting up the hooks after the fact, by iterating over all
+     OkHttpClient instances to do the same overrides. Lastly, for good measure,
+     it overrides the CertificatePinner::check method in case the pinner is
+     somehow changed by the app at runtime. This change also ends up negating
+     the following used when building n OkHttpClient by default from a builder.
+     This is done as it is more complicated to implement this behavior
+     after-the-fact.
+
+     ```
+     this.certificatePinner = builder
+                             .certificatePinner
+                             .withCertificateChainCleaner(
+                               certificateChainCleaner
+                             );
+     ```
+
+     Note: As of Android 8.0 "Oreo," Android still uses OkHttp 2.x internally.
+  */
+  if (cfg === undefined) {
+    var cfg = {};
+  }
+  if (cfg !== undefined) {
+    if (cfg["proxy"] === undefined) {
+      cfg["proxy"] = {};
+    }
+    if (cfg["proxy"]["host"] === undefined) {
+      cfg["proxy"]["host"] = "127.0.0.1";
+    }
+    if (cfg["proxy"]["port"] === undefined) {
+      cfg["proxy"]["port"] = 8080;
+    }
+    if (cfg["pkg"] === undefined) {
+      cfg["pkg"] = "okhttp3";
+    }
+    if (cfg["certificatePinnerClass"] === undefined) {
+      cfg["certificatePinnerClass"] = "CertificatePinner";
+    }
+    if (cfg["certificatePinner_DEFAULT"] === undefined) {
+      cfg["certificatePinner_DEFAULT"] = "DEFAULT";
+    }
+    if (cfg["okHttpClientClass"] === undefined) {
+      cfg["okHttpClientClass"] = "OkHttpClient";
+    }
+    if (cfg["okHttpClientBuilderClass"] === undefined) {
+      cfg["okHttpClientBuilderClass"] = "OkHttpClient$Builder";
+    }
+    if (cfg["okHttpClient_certificatePinnerField"] === undefined) {
+      cfg["okHttpClient_certificatePinnerField"] = "certificatePinner";
+    }
+    if (cfg["okHttpClient_proxyField"] === undefined) {
+      cfg["okHttpClient_proxyField"] = "proxy";
+    }
+  }
+  if (cfg["certificatePinnerClass"].search('.') != -1) {
+    cfg["certificatePinnerClass"] = cfg["pkg"] + '.' + cfg["certificatePinnerClass"];
+  }
+  if (cfg["okHttpClientClass"].search('.') != -1) {
+    cfg["okHttpClientClass"] = cfg["pkg"] + '.' + cfg["okHttpClientClass"];
+  }
+  if (cfg["okHttpClientBuilderClass"].search('.') != -1) {
+    cfg["okHttpClientBuilderClass"] = cfg["pkg"] + '.' + cfg["okHttpClientBuilderClass"];
+  }
+
+  var pinnerClass = Java.use(cfg["certificatePinnerClass"]);
+  var clientClass = Java.use(cfg["okHttpClientClass"]);
+  var ProxyClass = Java.use("java.net.Proxy");
+  var ProxyTypeClass = Java.use("java.net.Proxy$Type");
+  var InetSocketAddressClass = Java.use("java.net.InetSocketAddress");
+
+  var proxy = ProxyClass.$new(
+    ProxyTypeClass.valueOf("HTTP"),
+    InetSocketAddressClass.$new(cfg["proxy"]["host"], cfg["proxy"]["port"])
+  );
+  var defaultPinner = pinnerClass[cfg["certificatePinner_DEFAULT"]].value;
+
+  clientClass.$init.overload(
+    cfg["okHttpClientBuilderClass"]
+  ).implementation = function(builder) {
+    this.$init(builder);
+    this[
+      cfg["okHttpClient_certificatePinnerField"]
+    ].value = defaultPinner;
+    this[
+      cfg["okHttpClient_proxyField"]
+    ].value = proxy;
+    return this;
+  };
+
+  Java.choose(cfg["okHttpClientClass"], {
+    onMatch: function(instance) {
+      instance[
+        cfg["okHttpClient_certificatePinnerField"]
+      ].value = defaultPinner;
+      instance[
+        cfg["okHttpClient_proxyField"]
+      ].value = proxy;
+    },
+    onComplete: function() {}
+  });
+
+  pinnerClass.check.overload(
+    'java.lang.String', 'java.util.List'
+  ).implementation = function(hostname, peerCertificates) { };
+}
+
+
+function okhttp2(cfg) {
+  /* This function will override the OkHttpClient constructor to inject a Proxy
+     into the built OkHttpClient and forcibly set its CertificatePinner to the
+     default blank one. As OkHttp 2.x has a mutable OkHttpClient class, we disable
+     the setters for these. While this can be run through spawn gating, it also
+     supports setting up the hooks after the fact, by iterating over all
+     OkHttpClient instances to do the same overrides. Lastly, for good measure,
+     it overrides the CertificatePinner::check method in case the pinner is
+     somehow changed by the app at runtime.
+
+     If an app is trying to bypass the setters via reflection, just run this
+     function again to modify the instances or add a custom hook to deal with
+     their shenanigans.
+  */
+  if (cfg === undefined) {
+    var cfg = {};
+  }
+  if (cfg !== undefined) {
+    if (cfg["proxy"] === undefined) {
+      cfg["proxy"] = {};
+    }
+    if (cfg["proxy"]["host"] === undefined) {
+      cfg["proxy"]["host"] = "127.0.0.1";
+    }
+    if (cfg["proxy"]["port"] === undefined) {
+      cfg["proxy"]["port"] = 8080;
+    }
+    if (cfg["pkg"] === undefined) {
+      cfg["pkg"] = "com.square.okhttp";
+    }
+    if (cfg["certificatePinnerClass"] === undefined) {
+      cfg["certificatePinnerClass"] = "CertificatePinner";
+    }
+    if (cfg["certificatePinner_DEFAULT"] === undefined) {
+      cfg["certificatePinner_DEFAULT"] = "DEFAULT";
+    }
+    if (cfg["okHttpClientClass"] === undefined) {
+      cfg["okHttpClientClass"] = "OkHttpClient";
+    }
+    if (cfg["okHttpClientBuilderClass"] === undefined) {
+      cfg["okHttpClientBuilderClass"] = "OkHttpClient$Builder";
+    }
+    if (cfg["okHttpClient_certificatePinnerField"] === undefined) {
+      cfg["okHttpClient_certificatePinnerField"] = "certificatePinner";
+    }
+    if (cfg["okHttpClient_proxyField"] === undefined) {
+      cfg["okHttpClient_proxyField"] = "proxy";
+    }
+    if (cfg["okHttpClient_setProxyMethod"] === undefined) {
+      cfg["okHttpClient_setProxyMethod"] = "setProxy";
+    }
+    if (cfg["okHttpClient_setCertificatePinnerMethod"] === undefined) {
+      cfg["okHttpClient_setCertificatePinnerMethod"] = "setCertificatePinner";
+    }
+  }
+  if (cfg["certificatePinnerClass"].search('.') != -1) {
+    cfg["certificatePinnerClass"] = cfg["pkg"] + '.' + cfg["certificatePinnerClass"];
+  }
+  if (cfg["okHttpClientClass"].search('.') != -1) {
+    cfg["okHttpClientClass"] = cfg["pkg"] + '.' + cfg["okHttpClientClass"];
+  }
+  if (cfg["okHttpClientBuilderClass"].search('.') != -1) {
+    cfg["okHttpClientBuilderClass"] = cfg["pkg"] + '.' + cfg["okHttpClientBuilderClass"];
+  }
+
+  var pinnerClass = Java.use(cfg["certificatePinnerClass"]);
+  var clientClass = Java.use(cfg["okHttpClientClass"]);
+  var ProxyClass = Java.use("java.net.Proxy");
+  var ProxyTypeClass = Java.use("java.net.Proxy$Type");
+  var InetSocketAddressClass = Java.use("java.net.InetSocketAddress");
+
+  var proxy = ProxyClass.$new(
+    ProxyTypeClass.valueOf("HTTP"),
+    InetSocketAddressClass.$new(cfg["proxy"]["host"], cfg["proxy"]["port"])
+  );
+  var defaultPinner = pinnerClass[cfg["certificatePinner_DEFAULT"]].value;
+
+  clientClass.$init.overload().implementation = function() {
+    this.$init();
+    this[
+      cfg["okHttpClient_certificatePinnerField"]
+    ].value = defaultPinner;
+    this[
+      cfg["okHttpClient_proxyField"]
+    ].value = proxy;
+    return this;
+  };
+  clientClass.$init.overload(
+    cfg["okHttpClientClass"]
+  ).implementation = function(client) {
+    this.$init(client);
+    this[
+      cfg["okHttpClient_certificatePinnerField"]
+    ].value = defaultPinner;
+    this[
+      cfg["okHttpClient_proxyField"]
+    ].value = proxy;
+    return this;
+  };
+
+  clientClass[
+    cfg["okHttpClient_setProxyMethod"]
+  ].implementation = function(_p) {
+    this[
+      cfg["okHttpClient_proxyField"]
+    ].value = proxy;
+    return this;
+  };
+  clientClass[
+    cfg["okHttpClient_setCertificatePinnerMethod"]
+  ].implementation = function(_cp) {
+    ret[
+      cfg["okHttpClient_certificatePinnerField"]
+    ].value = defaultPinner;
+    return this;
+  };
+
+  Java.choose(cfg["okHttpClientClass"], {
+    onMatch: function(instance) {
+      instance[
+        cfg["okHttpClient_certificatePinnerField"]
+      ].value = defaultPinner;
+      instance[
+        cfg["okHttpClient_proxyField"]
+      ].value = proxy;
+    },
+    onComplete: function() {}
+  });
+
+  pinnerClass.check.overload(
+    'java.lang.String', 'java.util.List'
+  ).implementation = function(hostname, peerCertificates) { };
+}
+
+
+function deprecatedHttpClient(cfg) {
+  /* Unfortunately there are those who still use the deprecated Apache
+     HttpClient 4.x library (older, non-builder style) in their Android apps.
+     Well, at least it's better than HttpClient 3.x. That was a horror show.
+
+     As a part of Android itself (even if Google tries to pretend like it's not
+     in Oreo), this is generally the real one. However, if anyone decides to
+     bundle their own proguarded version (or if Google make the gradle dep use
+     an actual jar) we need to support setting the pkg/class/method names.
+  */
+  if (cfg === undefined) {
+    var cfg = {};
+  }
+  if (cfg !== undefined) {
+    if (cfg["proxy"] === undefined) {
+      cfg["proxy"] = {};
+    }
+    if (cfg["proxy"]["host"] === undefined) {
+      cfg["proxy"]["host"] = "127.0.0.1";
+    }
+    if (cfg["proxy"]["port"] === undefined) {
+      cfg["proxy"]["port"] = 8080;
+    }
+    if (cfg["abstractHttpClientClass"] === undefined) {
+      cfg["abstractHttpClientClass"] = "org.apache.http.impl.client.AbstractHttpClient";
+    }
+    if (cfg["httpParamsIface"] === undefined) {
+      cfg["httpParamsIface"] = "org.apache.http.params.HttpParams";
+    }
+    if (cfg["httpParamsIface_setParameterMethod"] === undefined) {
+      cfg["httpParamsIface_setParameterMethod"] = "setParameter";
+    }
+    if (cfg["httpParamsIface_getParameterMethod"] === undefined) {
+      cfg["httpParamsIface_getParameterMethod"] = "getParameter";
+    }
+    if (cfg["httpHostClass"] === undefined) {
+      cfg["httpHostClass"] = "org.apache.http.HttpHost";
+    }
+  }
+
+  var proxyClass = Java.use(cfg["httpHostClass"]);
+  var proxy = proxyClass.$new(cfg["proxy"]["host"],
+                              cfg["proxy"]["port"],
+                              "http"
+  );
+
+  var HttpParamsIface = Java.use(cfg["httpParamsIface"]);
+  var ModifierClass = Java.use("java.lang.reflect.Modifier");
+  var ABSTRACT = ModifierClass.ABSTRACT.value;
+  var INTERFACE = ModifierClass.INTERFACE.value;
+  var DEFAULT_PROXY = "http.route.default-proxy";
+
+  Java.enumerateLoadedClasses({
+    onMatch: function(className) {
+      try {
+        var clazz = Java.use(className);
+      } catch (e) {
+        return;
+      }
+      var mods = clazz.class.getModifiers();
+      if ((mods & INTERFACE) != 0) {
+        return;
+      }
+
+      if (HttpParamsIface.class.isAssignableFrom(clazz.class)) {
+        try {
+          clazz[cfg["httpParamsIface_setParameterMethod"]].overload(
+            'java.lang.String', 'java.lang.Object'
+          ).implementation = function(name, value) {
+            if (name == DEFAULT_PROXY) {
+              return this[
+                cfg["httpParamsIface_setParameterMethod"]
+              ](DEFAULT_PROXY, proxy);
+            } else {
+              return this[
+                cfg["httpParamsIface_setParameterMethod"]
+              ](name, value);
+            }
+          };
+        } catch (e) {}
+        try {
+          clazz[
+            cfg["httpParamsIface_getParameterMethod"]
+          ].implementation = function(name) {
+            //console.log('getParameter("' + name + '"):');
+            var ret = this[cfg["httpParamsIface_getParameterMethod"]](name);
+            //console.log(ret);
+            if (name == DEFAULT_PROXY) {
+              //console.log("returning our proxy");
+              return proxy;
+            }
+            return ret;
+          };
+        } catch (e) {}
+
+        if ((mods & ABSTRACT) == 0) {
+          Java.choose(className, {
+            onMatch: function(instance) {
+              try {
+                // our hook should catch this
+                instance[
+                  cfg["httpParamsIface_setParameterMethod"]
+                ](DEFAULT_PROXY, proxy);
+              } catch (e) { }
+            },
+            onComplete: function() {}
+          });
+        }
+      }
+    },
+    onComplete: function() {}
+  });
+
+  var clientClass = Java.use(cfg["abstractHttpClientClass"]);
+  clientClass.$init.implementation = function(conman, params) {
+    this.$init(conman, params);
+    // our hook should catch this
+    params[cfg["httpParamsIface_setParameterMethod"]](DEFAULT_PROXY, proxy);
+    return this;
+  };
+}
+
+
+function killTrust() {
+  var X509TrustManagerIface = Java.use("javax.net.ssl.X509TrustManager");
+  var HostnameVerifierIface = Java.use("javax.net.ssl.HostnameVerifier");
+  var X509ExtendedTrustManagerClass = null;
+  if (parseFloat(Java.androidVersion) >= 7.0) {
+    X509ExtendedTrustManagerClass = Java.use(
+      "javax.net.ssl.X509ExtendedTrustManager"
+    );
+  }
+  var ModifierClass = Java.use("java.lang.reflect.Modifier");
+  var ABSTRACT = ModifierClass.ABSTRACT.value;
+  var INTERFACE = ModifierClass.INTERFACE.value;
+
+  Java.enumerateLoadedClasses({
+    onMatch: function(className) {
+      try {
+        var clazz = Java.use(className);
+      } catch (e) {
+        return;
+      }
+
+      var mods = clazz.class.getModifiers();
+      if ((mods & INTERFACE) != 0) {
+        return;
+      }
+
+      if (X509TrustManagerIface.class.isAssignableFrom(clazz.class)) {
+        if ((mods & ABSTRACT) != 0) {
+          return;
+        }
+        try {
+          // iterating is a hack to get around
+          // com.android.org.conscrypt.TrustManagerImpl's nonstandard
+          // check*Trusted methods, though this will probably make up for
+          // the X509TrustManager/X509ExtendedTrustManager discrepancies
+          // across different android versions...
+          clazz.checkClientTrusted.overloads.forEach(function(overload) {
+            overload.implementation = function() { return null; };
+          });
+          clazz.checkServerTrusted.overloads.forEach(function(overload) {
+            overload.implementation = function() { return null; };
+          });
+          clazz.getAcceptedIssuers.implementation = function() { return []; };
+
+          /*clazz.checkClientTrusted.overload(
+            '[Ljava.security.cert.X509Certificate;', 'java.lang.String'
+          ).implementation = function(chain, authType) { };
+          clazz.checkServerTrusted.overload(
+            '[Ljava.security.cert.X509Certificate;', 'java.lang.String'
+          ).implementation = function(chain, authType) { };
+          */
+          //if (className == "com.android.org.conscrypt.TrustManagerImpl") {
+          //  clazz.checkTrustedRecursive.implementation = function(a,b,c,d,e,f){};
+          //}
+        } catch (e) {
+          console.log("failed to hook X509TrustManager methods for: " + className);
+        }
+      }
+
+      if (HostnameVerifierIface.class.isAssignableFrom(clazz.class)) {
+        try {
+          // due to frida being anti-polymorphic, we need to find every class
+          // that implements HostnameVerifier* and kill each of the verify
+          // methods they individually define
+          clazz.verify.overloads.forEach(function(overload) {
+            try {
+              if (overload.returnType.name === "V") {
+                overload.implementation = function() { };
+              } else if (overload.returnType.name === "Z") {
+                overload.implementation = function() {
+                  return true;
+                };
+              } else {
+                console.log("????");
+              }
+            } catch (e) { console.log(e); }
+          });
+        } catch (e) { console.log(e); }
+      }
+
+      /*
+      if (X509ExtendedTrustManagerClass != null &&
+          X509ExtendedTrustManagerClass.class.isAssignableFrom(clazz.class)) {
+        clazz.checkClientTrusted.overload(
+          '[Ljava.security.cert.X509Certificate;',
+          'java.lang.String', 'java.net.socket'
+        ).implementation = function(chain, authType, socket) { };
+
+        clazz.checkClientTrusted.overload(
+          '[Ljava.security.cert.X509Certificate;',
+          'java.lang.String', 'javax.net.ssl.SSLEngine'
+        ).implementation = function(chain, authType, engine) { };
+
+        clazz.checkServerTrusted.overload(
+          '[Ljava.security.cert.X509Certificate;',
+          'java.lang.String', 'java.net.socket'
+        ).implementation = function(chain, authType, socket) { };
+
+        clazz.checkServerTrusted.overload(
+          '[Ljava.security.cert.X509Certificate;',
+          'java.lang.String', 'javax.net.ssl.SSLEngine'
+        ).implementation = function(chain, authType, engine) { };
+      }
+      */
+    },
+    onComplete: function() {}
+  });
+}
+
+
+function trustslayer(cfg) {
+  console.log("slaying all trust");
+  Java.perform(function() {
+    
+    killTrust();
+    
+    try {
+      okhttp3(cfg);
+    } catch (e) { }
+
+    try {
+      okhttp2(cfg);
+    } catch (e) { }
+
+    try {
+      var android_cfg = { "pkg": "com.android.okhttp" };
+      if (cfg["proxy"]) {
+        android_cfg["proxy"] = cfg["proxy"];
+      }
+      okhttp2(android_cfg);
+      //okhttp2({ "pkg": "com.android.okhttp" });
+    } catch (e) { }
+    
+    try {
+      deprecatedHttpClient(cfg);
+    } catch (e) { }
+  });
+}
+
+rpc.exports = {
+  'all': function(cfg) {
+    Java.perform(function() {
+      trustslayer(cfg);
+    });
+  },
+  'killTrust': function() {
+    Java.perform(function() {
+      killTrust();
+    });
+  },
+  'okhttp3': function(cfg) {
+    Java.perform(function() {
+      okhttp3(cfg);
+    });
+  },
+  'okhttp2': function(cfg) {
+    Java.perform(function() {
+      okhttp2(cfg);
+    });
+  },
+  'okhttp2_android': function(cfg) {
+    Java.perform(function() {
+      var android_cfg = { "pkg": "com.android.okhttp" };
+      if (cfg["proxy"]) {
+        android_cfg["proxy"] = cfg["proxy"];
+      }
+      okhttp2(android_cfg);
+    });
+  },
+  'apache4': function(cfg) {
+    Java.perform(function() {
+      deprecatedHttpClient(cfg);
+    });
+  },
+}
---
+++ frida-trustslayer/bin/cli.js
@@ -0,0 +1,258 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const version = require('../package.json').version;
+const fs = require('fs');
+const frida = require('frida');
+const path = require('path');
+const { promisify } = require('util')
+const pReadFile = promisify(fs.readFile);
+
+
+let device, platform, target, paused, config, hooks;
+
+main().catch((err)=>{
+  console.err(err)
+});
+
+async function main() {
+  program
+    .version(version)
+    .usage('[options] <hook type>...')
+    .option('-t', '--type [platform]', 'defaults to "android"')
+    .option('-s, --spawn [app]', 'spawn app')
+    .option('-g, --gate [app]', 'spawn app with spawn gating')
+    .option('-p, --pid [pid]', 'attach to pid')
+    .option('-x, --proxy [host:port]', 'set http proxy. (127.0.0.1:8080)')
+    .option('-r, --remote [host:port]', 'connect to remote frida-server')
+    .option('-u, --usb', 'connect to USB device')
+    .option('-o, --obfuscated [mapping.json]', 'use deobfuscation mapping')
+    .parse(process.argv);
+
+  platform = program.platform || 'android';
+  hooks = program.args || ['all'];
+  config = { }
+
+  if (platform == 'android') {
+    if (!!program.spawn + !!program.gate + !!program.pid > 1) {
+      console.err("Error: Only one of of --spawn/--gate/--pid may be specified.");
+      program.help();
+    }
+    if (!!program.spawn + !!program.gate + !!program.pid < 1) {
+      console.err("Error: One of --spawn/--gate/--pid must be specified.");
+      program.help();
+    }
+    if (!!program.remote + !!program.usb > 1) {
+      console.err("Error: Only one of --remote/--usb may be specified.");
+      program.help();
+    }
+
+    if (!!program.remote + !!program.usb) {
+      if (program.gate) {
+        console.err("Error: --gate may only be used with a compatible device specified via --usb/--remote.")
+        program.help();
+      }
+    }
+
+    if (program.proxy) {
+      if (program.proxy === true) {
+        console.err("Error: proxy must be specified for --proxy.");
+        program.help();
+      }
+      let proxy_split = program.proxy.split(':');
+      if (proxy_split.length != 2) {
+        console.err("Error: Invalid proxy format does not match host:port.");
+        program.help();
+      }
+      let proxy_port = parseInt(proxy_split[1]);
+      if (isNaN(pid) || proxy_port < 0 || proxy_port > 65535) {
+        console.err("Error: Invalid proxy port");
+        program.help();
+      }
+      config["proxy"] = {
+        "host": proxy_split[0],
+        "port": proxy_port
+      }
+    }
+
+    let obfuscated = {};
+    /* this format is not actually used (yet)
+      {
+        'okhttp3': {
+          'pkg': 'a.b.c',
+          'classes': {
+            'CertificatePinner': {
+              'name': '...',
+              'methods': {},
+              'fields': {
+               'DEFAULT': '...',
+              }
+            },
+            'OkHttpClient': {
+              'name': '...',
+              'methods': {
+                'check': {
+                  'name': '...',
+                  'arglist': []
+                }
+              },
+              'fields': {
+                'certificatePinner': '...',
+                'proxy': '...'
+              }
+            },
+            'OkHttpClient$Builder': {
+              'name': '...',
+            }
+          }
+        }
+      }
+    */
+    if (program.obfuscated) {
+      try {
+        obfuscated = JSON.parse(fs.readFileSync(program.obfuscated, 'utf-8'));
+      } catch (err) {
+        console.err(err);
+        process.exit(1);
+      }
+    }
+    config['obfuscated'] = obfuscated;
+
+    if (program.usb) {
+      device = await frida.getUsbDevice();
+    } else if (program.remote) {
+      if (program.remote === true) {
+        device = await frida.getRemoteDevice()
+      } else {
+        let mgr = frida.getDeviceManager();
+        device = await mgr.addRemoteDevice(program.remote);
+      }
+    }
+
+    if (device === null) {
+      device = frida;
+    }
+
+    if (program.spawn && program.spawn !== true) {
+      target = program.spawn;
+      paused = true;
+      let pid;
+      try {
+        pid = await device.spawn([target]);
+      } catch (err) {
+        console.err(err);
+        process.exit(1);
+      }
+      await handle_pid(pid);
+      await device.resume(pid);
+    } else if (program.gate && program.gate !== true) {
+      target = program.gate;
+      try {
+        device.events.listen('spawned', on_spawned);
+        await device.enableSpawnGating();
+        console.log('Waiting for process ' + target + ' to be started.');
+      } catch (err) {
+        console.err(err);
+        process.exit(1);
+      }
+    } else if (program.pid && program.pid !== true) {
+      paused = false;
+      if (program.pid === true) {
+        console.err("Error: pid must be specified for --pid.");
+        program.help();
+      }
+      let pid = parseInt(program.pid);
+      if (isNaN(pid) || pid < 0) {
+        console.err("Error: Invalid --pid");
+        program.help();
+      } else if (pid === 0 && program.pid != "0") {
+        console.err("Error: Possible error with provided --pid of 0.");
+        program.help();
+      }
+      handle_pid(pid);
+    } 
+  }
+}
+
+async function on_spawned(spawn) {
+  if (spawn.identifier === target) {
+    await device.disableSpawnGating();
+    paused = true;
+    await handle_pid(spawn.pid);
+  }
+  await device.resume(spawn.pid);
+}
+
+async function profile_device(session) {
+  const script = await session.createScript(`
+    var is_android = false;
+    Process.enumerateModules({
+      onMatch: function(mod) {
+        if (mod.name === "libandroid_runtime.so"
+            && mod.path.startsWith("/system")) {
+          is_android = true;
+          return "stop";
+        }
+      },
+      onComplete: function() {}
+    });
+    send({
+      'platform': Process.platform,
+      'has_java': Java.available,
+      'is_android': is_android
+    });
+  `);
+  let p = new Promise((res,rej)=>{
+    script.events.listen('message', (message, data) => {
+      if (message.type == "send") {
+        res(message.payload);
+      }
+    });
+  });
+  await script.load();
+  let profile = await p;
+  await script.unload();
+  return profile;
+}
+
+function validate_profile(profile) {
+  if (platform === "android") {
+    if (!profile.is_android || !profile.has_java) {
+      console.err("Error: Connected device is not Android.");
+      process.exit(1);
+    }
+  }
+}
+
+async function load_agent(session) {
+  const agent_path = path.join(__dirname, '../basic_agent.js');
+  const agent_code = await pReadFile(agent_path, 'utf-8');
+  const script = await session.createScript(agent_code);
+  await script.load();
+  return script;
+}
+
+async function handle_pid(pid) {
+  let session;
+  try {
+    session = await device.attach(pid);
+  } catch (err) {
+    console.err(err);
+    process.exit(1);
+  }
+  let profile = await profile_device(session);
+  validate_profile(profile);
+  const agent_script = await load_agent(session);
+  const TrustSlayer = await agent_script.getExports();
+  for (var h of hooks) {
+    console.log('Running hook "' + h + '"');
+    try {
+      await TrustSlayer[h](config);
+    } catch (err) {
+      console.err(err);
+    }
+  }
+  console.log("Finished configuring hooks!");
+  console.log("Leave this command running until you want to unload the hooks.");
+  process.stdin.resume();
+}
